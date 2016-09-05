0f9cc5600cdae7bb2c2f057778a7e4a13825d5e3
Mikołaj Leśny <MakaszML@gmail.com>
2016-09-05 10:40:20 +0200

    Final app

--- game3.js
+++ game3.js
@@ -3,3 +3,20 @@ var curTable;
 $.get('https://openexchangerates.org/api/latest.json', {app_id: 'b54fc8736f7142118122fddb2c9dc837'}, function(data){
   curTable = JSON.stringify(data);
 });
+
+
+
+$(".currencyIn").keyup(function(){
+  amount = ($(this).val());
+});
+$(".currency").keyup(function(){
+  currency = ($(this).val());
+  console.log(amount + currency);
+});
+$(".currencyIn3").keyup(function(){
+  fcurrency = ($(this).val());
+});
+
+$(".currencyOut").click(function(){
+  $(".currencyOutP").text((JSON.parse(curTable).rates[currency]*amount/(JSON.parse(curTable).rates[fcurrency])*amount).toFixed(2));
+})
